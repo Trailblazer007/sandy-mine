5f302ee6dbd32e580f464d90151ab6cbb99cef5f
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2024-02-19 12:22:50 +0100

    dashboard created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="py-4 font-bold text-lg text-center">
+      <p>Welcome John</p>
+    </div>
+  );
 }
