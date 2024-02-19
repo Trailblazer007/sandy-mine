47a38fadbec9c9c87c567b7247c43a2426d79098
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2024-02-19 13:19:54 +0100

    made changes to the signUp page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Link from "next/link";
 
 const page = () => {
   return (
     <div className="bg-blue-100">
       <div className="p-4 text-right">
-        <p>Sign Up</p>
+        <p>Login</p>
       </div>
 
       <div className=" h-screen w-auto flex justify-center items-center">
         <div>
           <div className="bg-white p-6 border-2 border-transparent rounded-lg shadow-lg">
-            <p className="py-4">Login</p>
+            <p className="py-4">Sign Up</p>
             <form action="">
               <div className="flex py-2">
                 <p>Email:</p>
@@ -24,13 +23,14 @@ const page = () => {
                   <input type="text" placeholder="enter your password" />
                 </div>
 
+                <div className="flex">
+                  <p>Password:</p>
+                  <input type="text" placeholder="confirm password" />
+                </div>
+
                 <button className="mt-2 p-2 bg-green-400 rounded-lg text-white">
                   Submit
                 </button>
-
-                <div className="text-blue-300 text-right mt-4">
-                  <Link href="../forgotPassword">forgot password?</Link>
-                </div>
               </div>
             </form>
           </div>
